af0dd8b4ab36a985b4468e2ffe9bfa5d0e55c544
CHELSEAS <chelsea.murphy@shecodes.com.au>
2025-11-22 12:52:32 +1000

    authentication added

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -2,6 +2,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -23,6 +25,22 @@ class CustomUserList(APIView):
             serializer.errors, 
             status=status.HTTP_400_BAD_REQUEST
         )
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        })
 
 class CustomUserDetail(APIView):
     def get_object(self, pk):
