ce64b351bfe0dd2d47270602ad30d0357de4bced
CHELSEAS <chelsea.murphy@shecodes.com.au>
2025-11-22 11:49:19 +1000

    Adding users

--- crowdfunding/fundraisers/serializers.py
+++ crowdfunding/fundraisers/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from django.apps import apps
 
 class PledgeSerializer(serializers.ModelSerializer):
+    supporter = serializers.ReadOnlyField(source='supporter.id')
+
     class Meta:
         model = apps.get_model('fundraisers.Pledge')
         fields = '__all__'
 
 class FundraiserSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.id')
+
     class Meta:
         model = apps.get_model('fundraisers.Fundraiser')
         fields = '__all__'
