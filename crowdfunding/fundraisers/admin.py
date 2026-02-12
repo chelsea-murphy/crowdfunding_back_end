c7ef3deef41df277e8102408b8564b3b3d79e51a
CHELSEAS <chelsea.murphy@shecodes.com.au>
2026-02-12 15:12:46 +1000

    fix admin registration - move user to users app

--- crowdfunding/fundraisers/admin.py
+++ crowdfunding/fundraisers/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.contrib import admin
 from .models import Fundraiser, Pledge
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -20,11 +18,3 @@ class PledgeAdmin(admin.ModelAdmin):
     list_filter = ('anonymous', 'date_created')
     search_fields = ('fundraiser__title', 'supporter__username', 'comment')
     ordering = ('-date_created',)
-
-# Register User (if using custom user model)
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    ordering = ('-date_joined',)
--- crowdfunding/users/admin.py
+++ crowdfunding/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+    
+    # Add this to make password reset easier
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
